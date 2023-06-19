b31370e5518f9533f246478b08394a124137c577
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-19 19:08:37 -0300

    refactor: Implement the three new components and update the text props to English.

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import QRCodeLink from "qrcode";
+import Title from "../components/Title";
 import Search from "../components/Search";
 import QRCodeImage from "../components/QRCodeImage";
-import Wrapper from "../components/Wrapper"
-
+import Wrapper from "../components/Wrapper";
 
 const Index = () => {
   const [QRCodeUrl, setQRCodeUrl] = useState("");
@@ -21,16 +21,21 @@ const Index = () => {
   };
 
   return (
-    <Wrapper>
-      <QRCodeImage value={QRCodeUrl} />
-      <Search
-        placeholder="Informe o link ou mensagem."
-        onChange={(e) => handleQRCode(e)}
-        href={QRCodeUrlDownload}
-        download="QRCode.png"
-        buttonText="Gerar QRCode"
-      />
-    </Wrapper>
+    <section>
+      <Wrapper>
+        <Title
+          title="QR Code Generator"
+          subtitle="Paste a link or message to create a QR Code"
+        />
+        <Search
+          placeholder="link or Message"
+          onChange={(e) => handleQRCode(e)}
+          href={QRCodeUrlDownload}
+          download="QRCode.png"
+        />
+        <QRCodeImage value={QRCodeUrl} />
+      </Wrapper>
+    </section>
   );
 };
 
