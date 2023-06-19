d1054b51c9829225394d4238c1d47098a6a037b6
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-19 20:21:46 -0300

    refactor: Remove unnecessary space between lines.

--- src/components/QRCodeImage.jsx
+++ src/components/QRCodeImage.jsx
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 import QRCode from 'react-qr-code';
 
 const StyledQRCode = styled(QRCode)`
-
+    max-width: 256px;
+    max-height: 256px;
 `;
 
 const QRCodeWidget = ({ value }) => {
--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 
-
-
 const Wrapper = styled.div`
-    
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 8px;
 `;
 
 export default Wrapper;
