cda8e8eb702fa7ba94e1fbdbe1a9d8794c8df87d
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-20 00:28:09 -0300

    refactor: In index.jsx, modify the code to only pass the title and subtitle of the widget through props and render the Search and Render components accordingly.

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,39 +1,38 @@
 import React, { useState } from "react";
-import QRCodeLink from "qrcode";
+import Wrapper from "../components/Wrapper";
+import SwitchPage from "../components/SwitchPage";
 import Title from "../components/Title";
 import Search from "../components/Search";
-import QRCodeImage from "../components/QRCodeImage";
-import Wrapper from "../components/Wrapper";
+import Reader from "../components/Reader";
+
 
 const Index = () => {
-  const [QRCodeUrl, setQRCodeUrl] = useState("");
-  const [QRCodeUrlDownload, setQRCodeUrlDownload] = useState("");
-
-  const handleQRCode = (e) => {
-    setQRCodeUrl(e.target.value);
-    handleQRCodeLink(e.target.value);
-  };
-
-  const handleQRCodeLink = (urlInput) => {
-    QRCodeLink.toDataURL(urlInput, { width: 600, margin: 3 }, (err, url) => {
-      setQRCodeUrlDownload(url);
-    });
-  };
+  const [switchedPage, setSwitchedPage] = useState(true);
 
   return (
     <section>
       <Wrapper>
-        <Title
-          title="QR Code Generator"
-          subtitle="Paste a link or message to create a QR Code"
+        <SwitchPage
+          onClick={() => setSwitchedPage(!switchedPage)}
+          pageStatus={switchedPage}
         />
-        <Search
-          placeholder="link or Message"
-          onChange={(e) => handleQRCode(e)}
-          href={QRCodeUrlDownload}
-          download="QRCode.png"
-        />
-        <QRCodeImage value={QRCodeUrl} />
+        {switchedPage ? (
+          <>
+            <Title
+              title="QR Code Generator"
+              subtitle="Paste a link or message to create a QR Code"
+            />
+            <Search />
+          </>
+        ) : (
+          <>
+            <Title
+              title="QR Code Reader"
+              subtitle="Select a QR Code to read it"
+            />
+            <Reader />
+          </>
+        )}
       </Wrapper>
     </section>
   );
